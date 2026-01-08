33d275c9cfa02ce53d299951998ad9633d094cb7
Lui <ti@akiyama.com.br>
2026-01-08 20:59:26 -0300

    v2.0.9 - Esconde header do tema nas paginas do blog

--- blog.php
+++ blog.php
@@ -3,7 +3,7 @@
  * Plugin Name: Blog PDA
  * Plugin URI: https://github.com/pereira-lui/blog
  * Description: Plugin de Blog personalizado para WordPress. Cria um Custom Post Type "Blog" com templates personalizados, suporte a importação e atualização automática via GitHub.
- * Version: 2.0.8
+ * Version: 2.0.9
  * Author: Lui
  * Author URI: https://github.com/pereira-lui
  * Text Domain: blog-pda
